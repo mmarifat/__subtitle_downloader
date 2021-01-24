6e289be5c8f99a6b86ed1a3073b0950b37de23a4
mmarifat <mma.rifat66@gmail.com>
2021-01-24 12:41:15 +0600

    minor change

--- quasar.conf.js
+++ quasar.conf.js
@@ -74,7 +74,7 @@ module.exports = configure(function (/* ctx */) {
 		// Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
 		devServer: {
 			clientLogLevel: "silent",
-			https: false,
+			https: true,
 			port: 9090,
 			open: true // opens browser window automatically
 		},
